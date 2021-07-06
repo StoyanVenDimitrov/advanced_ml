156eecc9e06bb693ac6121f3b7a07e672b4eca75
dimitrs <stoyan.dimitrov@dtms.de>
2021-07-06 14:46:18 +0200

    cd /Users/dimitrs/Development/stuttgart-university/advanced_ml ; /usr/bin/env /Users/dimitrs/Development/stuttgart-university/advanced_ml/venv/bin/python /Users/dimitrs/.vscode/extensions/ms-python.python-2021.6.944021595/pythonFiles/lib/python/debugpy/launcher 53448 -- /Users/dimitrs/Development/stuttgart-university/advanced_ml/src/algorithm.py

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,5 +1,31 @@
-import gym_minigrid
+import gym
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 from gym_minigrid.wrappers import *
 from dqn import DQN
 
-env = gym.make('MiniGrid-Empty-8x8-v0')
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+env = gym.make('MiniGrid-Empty-5x5-v0')
+env.reset()
+before_img = env.render('rgb_array')
+
+# take an action and render the resulting state
+action = env.actions.forward
+obs, reward, done, info = env.step(action)
+after_img = env.render('rgb_array')
+
+plt.imshow(np.concatenate([before_img, after_img], 1))
+
+
+# for i in range(100):
+#     action = env.action_space
+#     obs, reward, done, info = env.step(action.sample())
+
+#     env.render()
+#     if done:
+#         obs = env.reset()
